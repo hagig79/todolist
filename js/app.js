abe3d78de4a448cb88417656878d4896baf5697a
hagio <hagio@rrr-z.co.jp>
2018-03-22 16:42:04 +0900

    Add click event

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 var result = navigator.serviceWorker.register('sw.js', {scope: '/todolist/'});
+
+$(function () {
+    $('#btn-save').click(function () {
+        console.log($('#text-input').val());
+    });
+});
